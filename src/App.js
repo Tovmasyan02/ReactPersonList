3cb91e20bed5b7e42c47eb291e8516487c80056b
Armen <armentovmasyan02@gmail.com>
2019-08-10 15:36:47 +0300

    создал новый проект

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import './App.css';
-import Header from './Header/Header.js';
-import Content from './Content/Content.js';
-import Footer from './Footer/Footer.js';
+
+
 function App() {
   return (
     <div>
-    <Header/>
-    <Content/>
-    <Footer/>
+    
     </div>
   );
 }
